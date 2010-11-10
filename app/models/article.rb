991425d447d780e37ec6a407a0f432155a82d840
Pascal Toutain <pascaltoutain@gmail.com>
2010-11-10 14:25:38 +0100

    Annotation des models (avec Annotate)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+# Schema version: 20101110121455
+#
+# Table name: articles
+#
+#  id          :integer(4)      not null, primary key
+#  nom         :string(255)
+#  commentaire :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Article < ActiveRecord::Base
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20101110121455
+#
+# Table name: regions
+#
+#  id         :integer(4)      not null, primary key
+#  nom        :string(255)
+#  code       :string(255)
+#  nc         :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Region < ActiveRecord::Base
   has_many :departements
 end
